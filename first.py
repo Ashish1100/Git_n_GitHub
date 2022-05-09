4710b2960dab49d8ebb6e8304d8a3277a332592c
Ashish1100 <97810143+Ashish1100@users.noreply.github.com>
2022-05-10 01:01:14 +0530

    My Work

--- first.py
+++ first.py
@@ -1,3 +1,73 @@
-#Display the output
-print("Hello Dear")
-print("hi")
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curriculum Vitae</title>
+</head>
+ <body>
+<center>
+    <h1>Ashish Saha</h1>
+    <pr>Chandigarh, India</pr><br>
+    <pr>Mobile: 9101910328</pr><br>
+    <pr>LinkedIn: linkedin.com/ashishsaha</pr><br>
+    <pr>Email: ashishsaha062 [at] gmail [dot] com</pr>
+</center> 
+<h3>Profile</h3>
+<pr><ul>The primary focus on my study is be in the field of Computer Science. I am a Software Development & Data Science enthusiast.</ul></h5>
+<h3>Education</h3>
+<ul>
+<h5><li>Chandigarh University, Mohali, Punjab, India</li></h5><br>
+ <pr>B.E. in Computer Science ............................................................................................................................. June, 2021 - June, 2025</pr><br>
+ <em><pr>Computer Science and Engineering with specialization in Big Data Analytics with IBM.</pr></em>
+ <h5><li>Annundoram Barooah Academy, Pathsala, Assam, India</li></h5>
+ <pr>H.S.C. in Science ........................................................................................................................................... May, 2017 - May, 2019</pr><br>
+ <em><pr>Higher Secondary or 12th Class in Science stream with PCM subjects.</pr></em>
+</ul>
+<h3>Experience</h3>
+<ul>
+    <h5><li>Campus Ambassador Intern</li></h5>
+    <pr>International Model United Nations Campus Ambassador Intership&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; May, 2022 - June, 2022</pr><br>
+    <em><pr>During the intership period I helped the organization in making its brand presence.</pr></em><br>
+    <em><pr>I participated and helped in brand promotion for the organization.</pr></em><br>
+    <em><pr>Helped the organization in its sales growth through referals.</pr></em><br>
+    
+</ul>
+<h3>Skills</h3>
+<ul>
+    <h5><li>Technical</li></h5>
+    <pr>Website Development</pr><br>
+    <pr>Project Management</pr><br>
+    <pr>Data Analytics</pr><br>
+    <pr>Linux & Commands</pr><br>
+    <pr>Git & GitHub</pr><br>
+    
+    
+    <h5><li>Programming Language</li></h5>
+    <pr>HTML, CSS and JavaScript</pr><br>
+    <pr>Python</pr><br>
+    <pr>Java</pr><br>
+    <pr>SQL</pr><br>
+    <pr>R</pr><br>
+    <h5><li>Personal</li></h5>
+    <pr>Time Management</pr><br>
+    <pr>Collaboration</pr><br>
+    <pr>Leadership</pr><br>
+    <pr>Teamwork</pr><br>
+    <h5><li>Other</li></h5>
+    <pr>Microsoft Powerpoint</pr><br>
+    <pr>Microsoft Excel</pr><br>
+    <pr>Content Writing</pr><br>
+    <pr>UI/UX</pr><br>
+   
+
+</ul>
+
+<h3>Training</h3>
+<ul>
+
+</ul>
+
+</body>
+</html>
